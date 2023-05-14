4d565cbe9cdee22c35240ac15018bc1f1747a93c
Rafael Ignacio Zurita <rafa@fi.uncoma.edu.ar>
2023-05-14 09:29:42 -0300

    xinu diagram bloq

--- device/mice/micehandler.c
+++ device/mice/micehandler.c
@@ -46,6 +46,7 @@ void finish_mice(void)
 }
 
 
+#define MOUSE_V_BIT  0x08
 void micehandler(uint8 data)
 {
 
@@ -56,6 +57,7 @@ void micehandler(uint8 data)
 		if (mouse_byte[0] == 0xAA) {		/* hot plug: re init mouse */
 			return;
 		}
+		if (!(data & MOUSE_V_BIT)) {mouse_cycle=0; return;}
 		mouse_cycle++;
 		break;
 	case 1:
@@ -71,5 +73,45 @@ void micehandler(uint8 data)
 		finish_mice();
 		break;
 	}
+}
+
+#define MOUSE_PORT   0x60
+#define MOUSE_STATUS 0x64
+#define MOUSE_ABIT   0x02
+#define MOUSE_BBIT   0x01
+#define MOUSE_WRITE  0xD4
+#define MOUSE_F_BIT  0x20
+#define MOUSE_V_BIT  0x08
+
+void micehandler2(uint8 data) {
+	uint8 status = inportb(MOUSE_STATUS);
+	mouse_byte[0] = data;
+	++mouse_cycle;
+	if (!(data & MOUSE_V_BIT)) {mouse_cycle=0; return;}
+
+	while (status & MOUSE_BBIT) {
+		uint8 mouse_in = inportb(MOUSE_PORT);
+		if (status & MOUSE_F_BIT) {
+			switch (mouse_cycle) {
+			case 0:
+				mouse_byte[0] = mouse_in;
+				if (!(mouse_in & MOUSE_V_BIT)) {mouse_cycle=0; return;}
+				++mouse_cycle;
+				break;
+			case 1:
+				mouse_byte[1] = mouse_in;
+				++mouse_cycle;
+				break;
+			case 2:
+				mouse_byte[2] = mouse_in;
+				break;
+			case 3:
+				mouse_byte[3]=mouse_in;
+				break;
+			}
+		}
+		status = inportb(MOUSE_STATUS);
+	}
+	finish_mice();
 }
 
