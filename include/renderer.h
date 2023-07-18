a4b81abca9b501577555dbaeec95b6547371db26
facundoMunoz <facundomunoz001@gmail.com>
2023-07-18 13:50:58 -0300

    Add mouse movement and left click functionality in renderer

--- include/renderer.h
+++ include/renderer.h
@@ -13,6 +13,13 @@ void r_set_clip_rect(mu_Rect rect);
 void r_clear(mu_Color color);
 void r_present(void);
 extern const unsigned short pointer[];
+struct mouse_state {
+	int pos_y;
+    int pos_x;
+    uint8 left_mouse;
+    uint8 right_mouse;
+};
+extern struct mouse_state m_state;
 
 void r_handle_input(mu_Context *ctx);
 #endif
