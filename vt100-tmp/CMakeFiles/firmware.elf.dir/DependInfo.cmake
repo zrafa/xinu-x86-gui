62fc4cddec1ee293aa168e0a17339f9dd6a56457
Rafael Ignacio Zurita <rafa@fi.uncoma.edu.ar>
2023-09-06 20:58:29 -0300

    vt100 tmp to port

---
+++ vt100-tmp/CMakeFiles/firmware.elf.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/tmp/t/avr-vt100/ili9340.c" "CMakeFiles/firmware.elf.dir/ili9340.c.o" "gcc" "CMakeFiles/firmware.elf.dir/ili9340.c.o.d"
+  "/tmp/t/avr-vt100/uart.c" "CMakeFiles/firmware.elf.dir/uart.c.o" "gcc" "CMakeFiles/firmware.elf.dir/uart.c.o.d"
+  "/tmp/t/avr-vt100/vt100.c" "CMakeFiles/firmware.elf.dir/vt100.c.o" "gcc" "CMakeFiles/firmware.elf.dir/vt100.c.o.d"
+  "/tmp/t/avr-vt100/demo.cpp" "CMakeFiles/firmware.elf.dir/demo.cpp.o" "gcc" "CMakeFiles/firmware.elf.dir/demo.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ vt100-tmp/CMakeFiles/firmware.elf.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firmware.elf.dir/demo.cpp.o"
+  "CMakeFiles/firmware.elf.dir/demo.cpp.o.d"
+  "CMakeFiles/firmware.elf.dir/ili9340.c.o"
+  "CMakeFiles/firmware.elf.dir/ili9340.c.o.d"
+  "CMakeFiles/firmware.elf.dir/uart.c.o"
+  "CMakeFiles/firmware.elf.dir/uart.c.o.d"
+  "CMakeFiles/firmware.elf.dir/vt100.c.o"
+  "CMakeFiles/firmware.elf.dir/vt100.c.o.d"
+  "firmware.elf"
+  "firmware.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/firmware.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
