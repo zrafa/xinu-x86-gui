fa0a123c47ad6e56f0cbf86bcedab717fc23fda2
Rafael Ignacio Zurita <rafa@fi.uncoma.edu.ar>
2024-02-16 17:39:51 -0300

    some time for microui to init

--- system/main.c
+++ system/main.c
@@ -10,6 +10,7 @@ process main(void)
 	recvclr();
 	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
 	resume(create(microui, 16384, 20, "microui", 0));
+	sleep(1); /* some time for microui to init */
 	resume(create(vt, 4096, 20, "vt 0", 0));
 	resume(create(mu_clock, 4096, 20, "clock", 0));
 
